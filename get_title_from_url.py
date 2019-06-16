ce2a3afcd925cea9d6c5beacab137fa9ca549c4b
Iskren <iskren.alexandrov@gmail.com>
2019-06-16 22:41:17 +0300

    Made basic search

--- get_title_from_url.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-import urllib.parse
-import re # regular expression
-
-# query_string = urllib.parse.urlencode({"search_query" : input()})
-html_content = urllib.request.urlopen("https://www.youtube.com/watch?v=7gh0Cyg2iEQ")
-search_results = re.findall('<title>(.*)</title>', html_content.read().decode())
-print(search_results[0])
---
+++ main.py
@@ -0,0 +1,24 @@
+import urllib.request
+import urllib.parse
+import re
+
+url = input()
+
+
+def get_title_by_url(url):
+    html_content = urllib.request.urlopen(url)
+    search_results = re.findall(
+        '<title>(.*)</title>', html_content.read().decode())
+
+    song_name = search_results[0].rsplit(' ', 2)[0]
+    print(song_name)
+
+    return song_name
+
+
+def search_osu(title):
+    search_url = urllib.parse.quote(title)
+    return 'https://osu.ppy.sh/beatmapsets?q=' + search_url
+
+
+print(search_osu(get_title_by_url(url)))
