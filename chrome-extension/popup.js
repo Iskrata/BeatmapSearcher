ef96520e9f429eda30f0771d23c7c6f732da0f05
Iskrata <Iskren.Alexandrov@gmail.com>
2019-06-17 20:23:15 +0300

    started working on chrome extension

---
+++ chrome-extension/popup.js
@@ -0,0 +1,7 @@
+function onPageDetailsReceived(pageDetails) {
+    document.getElementById('title').value = pageDetails.title;
+    document.getElementById('url').value = pageDetails.url;
+    document.getElementById('summary').innerText = pageDetails.summary;
+}
+
+alert(pageDetails.title)
